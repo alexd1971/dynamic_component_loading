74d4cc2aa2539c7f485a05f2362414f752e77876
Aleksey Danilevskiy <a.danilevsky@allfuneral.com>
2020-12-30 13:14:20 +0300

    fix

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:angular/angular.dart';
 
 import 'dynamic_component.dart';
@@ -5,22 +7,30 @@ import 'dynamic_component.template.dart' as ng;
 
 @Component(
   selector: 'my-app',
-  template: '<template #marker></template>',
+  template: '<div #app></div><button (click)="setName()">Click Me</button>',
   directives: [DynamicComponent],
 )
 class AppComponent implements AfterViewInit {
   final ComponentLoader _loader;
+  final Injector _injector;
 
-  AppComponent(this._loader);
+  AppComponent(this._loader, this._injector) {
+    _dynamicComponent = _loader.loadDetached(ng.DynamicComponentNgFactory, injector: _injector);
+  }
 
-  @ViewChild('marker', read: ViewContainerRef)
-  ViewContainerRef location;
+  @ViewChild('app')
+  DivElement app;
 
-  ComponentRef _dynamicComponent;
+  ComponentRef<DynamicComponent> _dynamicComponent;
 
   @override
   void ngAfterViewInit() {
-    _dynamicComponent =
-        _loader.loadNextToLocation(ng.DynamicComponentNgFactory, location);
+    app.append(_dynamicComponent.location);
+  }
+
+  void setName() {
+    _dynamicComponent.update((c) {
+        c.name = 'World';
+    });
   }
 }
